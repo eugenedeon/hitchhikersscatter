4dfaa81aa3a13195a1bd10ef643d45819ccadde6
EugeneDeon <e@8i.com>
2016-07-16 18:10:47 +1200

    infinite 3d medium, iso point source, iso scatter, MC code and nb analysis (incomplete)

---
+++ code/3D_medium/infinite3Dmedium/GENinfinite3Dmedium.h
@@ -0,0 +1,208 @@
+#include <util.h> // for discrete map
+#include <random.h>
+
+// GEN - 3D - INFINITE MEDIUM - base estimator for a variety of step-length distributions and phase functions
+
+class GENInfiniteMedium
+{
+public: 
+
+    virtual double samplestep() = 0; // step-length distribution sampling
+    virtual Vector3 sampledir(const Vector3& prevDir ) = 0; // direction sampling
+    virtual void printDescriptor() = 0; // print a string describing the medium's variety
+
+    // estimate fluence at various radii from an isotropic point source
+    void isotropicPointSourceAnalogMut(  const double c, // single-scattering albedo
+                                         const double maxr, // maximum radius to record
+                                         const double dr, // uniformly spaced radial bins of with dr
+                                         const double du, // uniformly spaced direction cosine bins
+                                         const size_t numsamples, 
+                                         const size_t numCollisionOrders,
+                                         const size_t numMoments
+                                      )
+    {
+        // collision density
+        const size_t num_r_bins = floor( maxr / dr ) + 1.0;
+        size_t * collisionDensity = new size_t [num_r_bins];
+        for( size_t i = 0; i < num_r_bins; ++i )
+        {
+            collisionDensity[i] = 0;
+        }
+
+        // angular collision density
+        const size_t num_u_bins = floor( 2.0 / du ) + 1.0;
+        const size_t num_ru_bins = num_r_bins * num_u_bins;
+        size_t * angularCollisionDensity = new size_t [num_ru_bins];
+        for( size_t i = 0; i < num_ru_bins; ++i )
+        {
+            angularCollisionDensity[i] = 0;
+        }
+
+        // collision density of arbitrary order
+        const size_t numCollisionbins = num_r_bins * numCollisionOrders;
+        size_t * collisionDensities = new size_t [numCollisionbins];
+        for( size_t i = 0; i < numCollisionbins; ++i )
+        {
+            collisionDensities[i] = 0;
+        }
+
+        // moments
+        double * globalMoments = new double [numMoments];
+        for( size_t i = 0; i < numMoments; ++i )
+        {
+            globalMoments[i] = 0;
+        }
+
+        const size_t numOrderMoments = numMoments * numCollisionOrders;
+        double * orderMoments = new double [numOrderMoments];
+        for( size_t i = 0; i < numOrderMoments; ++i )
+        {
+            orderMoments[i] = 0;
+        }
+
+        for( size_t i = 0; i < numsamples; ++i )
+        {
+            size_t collisionOrder = 0;
+
+            Vector3 pos( 0.0, 0.0, 0.0 );
+            Vector3 dir( 1.0, 0.0, 0.0 );
+
+            double step = samplestep();
+
+            pos += dir * step;
+
+            while( RandomReal() < c )
+            {
+                const double r = Norm( pos );
+                collisionOrder++; // we collided and didn't absorb
+
+                for( size_t m = 0; m < numMoments; ++m )
+                {
+                    globalMoments[m] += pow( r, double( m ) );
+                }
+
+                if( collisionOrder < numCollisionOrders )
+                {   
+                    for( size_t m = 0; m < numMoments; ++m )
+                    {
+                        assert( m * numCollisionOrders + collisionOrder < numOrderMoments );
+                        orderMoments[m * numCollisionOrders + collisionOrder] += pow( r, double( m ) );
+                    }
+                }
+
+                const size_t ri = std::min( num_r_bins - 1, (long unsigned int)( floor( r / dr ) ) );
+                assert( ri < num_r_bins );
+                collisionDensity[ri]++;
+
+                const float u = Dot( Normalize(pos), dir );
+                const size_t ui = discreteMap( -1.0, 1.0, du, u );
+                const size_t rui = num_u_bins * ri + ui;
+                assert( rui < num_ru_bins );
+                angularCollisionDensity[rui]++;
+
+                if( collisionOrder < numCollisionOrders )
+                {   
+                    const size_t collision_i = ri + num_r_bins * ( collisionOrder - 1 );
+                    assert( collision_i < numCollisionbins );
+                    collisionDensities[collision_i]++;
+                }
+
+                dir = sampledir( dir );
+                step = samplestep();
+                pos += dir * step;
+            }
+
+            collisionOrder++; // we collided and were absorbed
+
+            const double r = Norm( pos );
+            for( size_t m = 0; m < numMoments; ++m )
+            {
+                globalMoments[m] += pow( r, double( m ) );
+            }
+
+            if( collisionOrder < numCollisionOrders )
+            {   
+                for( size_t m = 0; m < numMoments; ++m )
+                {
+                    assert( m * numCollisionOrders + collisionOrder < numOrderMoments );
+                    orderMoments[m * numCollisionOrders + collisionOrder] += pow( r, double( m ) );
+                }
+            }
+            
+            const size_t ri = std::min( num_r_bins - 1, (long unsigned int)( floor( r / dr ) ) );
+            assert( ri < num_r_bins );
+            collisionDensity[ri]++;
+
+            const float u = Dot( Normalize(pos), dir );
+            const size_t ui = discreteMap( -1.0, 1.0, du, u );
+            const size_t rui = num_u_bins * ri + ui;
+            assert( rui < num_ru_bins );
+            angularCollisionDensity[rui]++;
+
+            if( collisionOrder < numCollisionOrders )
+            {   
+                const size_t collision_i = ri + num_r_bins * ( collisionOrder - 1 );
+                assert( collision_i < numCollisionbins );
+                collisionDensities[collision_i]++;
+            }
+        }
+
+        printDescriptor();
+        std::cout << "Analogmu_t c: " << c << 
+                     " maxr: " << maxr << 
+                     " dr: " << dr << 
+                     " du: " << du << 
+                     " numsamples: " << numsamples << 
+                     " numCollisionOrders: " << numCollisionOrders << 
+                     " numMoments: " << numMoments << 
+                     std::endl;
+
+        std::cout << "CollisionDensity:\n";
+        for( size_t i = 0; i < num_r_bins; ++i )
+        {
+            std::cout << double( collisionDensity[i] ) / ( double( numsamples ) * dr ) << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Density Moments:\n";
+        for( size_t m = 0; m < numMoments; m++ )
+        {
+            std::cout << globalMoments[m] / double( numsamples ) << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Per-collision Density Moments:\n";
+        for( size_t order = 0; order < numCollisionOrders; order++ )
+        {
+            for( size_t m = 0; m < numMoments; ++m )
+            {
+                std::cout << double( orderMoments[order + m * numCollisionOrders ] ) / double( numsamples ) << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "Per-collision Densities:\n";
+        for( size_t ci = 0; ci < numCollisionOrders; ++ci )
+        {
+            for( size_t ri = 0; ri < num_r_bins; ++ri )
+            {
+                const size_t collision_i = ri + num_r_bins * ( ci - 1 );
+                std::cout << double( collisionDensities[collision_i] ) / ( double( numsamples ) * dr ) << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout << "Angular Collision Densities:\n";
+        for( size_t ri = 0; ri < num_r_bins; ++ri )
+        {
+            for( size_t ui = 0; ui < num_u_bins; ++ui )
+            {
+                const size_t rui = ri * num_u_bins + ui;
+                std::cout << double( angularCollisionDensity[rui] ) / ( double( numsamples ) * dr * du ) << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        delete [] collisionDensity;
+    }
+};
---
+++ code/3D_medium/infinite3Dmedium/isotropicPointSource/infinite3D_isotropicpoint_isotropicscatter_exponential.cpp
@@ -0,0 +1,45 @@
+#include "../GENinfinite3Dmedium.h"
+
+class IsotropicExponentialInfiniteMedium : public GENInfiniteMedium
+{
+public:
+    double m_mut;
+    IsotropicExponentialInfiniteMedium(const double mut) : m_mut(mut){}
+
+    double samplestep() { return -log( RandomReal() ) / m_mut; }
+    Vector3 sampledir( const Vector3& prevDir ) // isotropic scattering
+    {
+        return isotropicDir();
+    }
+
+    void printDescriptor()
+    {
+        std::cout << "Infinite 3d isotropic point source isotropic scattering exponential random flight, mu_t = " << m_mut << std::endl;
+    }
+};
+
+int main( int argc, char** argv )
+{
+    srand48( time(NULL) );
+
+    if( argc != 9 )
+    {
+        std::cout << "usage: infiniteMedium c mut maxr dr du numsamples numCollisionOrders numMoments \n";
+        exit( -1 );
+    }
+
+    double c = StringToNumber<double>( std::string( argv[1] ) );
+    double mut = StringToNumber<double>( std::string( argv[2] ) );
+    double maxr = StringToNumber<double>( std::string( argv[3] ) );
+    double dr = StringToNumber<double>( std::string( argv[4] ) );
+    double du = StringToNumber<double>( std::string( argv[5] ) );
+    size_t numsamples = StringToNumber<size_t>( std::string( argv[6] ) );
+    size_t numCollisionOrders = StringToNumber<size_t>( std::string( argv[7] ) );
+    size_t numMoments = StringToNumber<size_t>( std::string( argv[8] ) );
+
+    IsotropicExponentialInfiniteMedium sampler(mut);
+
+    sampler.isotropicPointSourceAnalogMut( c, maxr, dr, du, numsamples, numCollisionOrders, numMoments );
+
+    return 0;
+}
