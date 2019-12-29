e1e09adc657920dc14247ee67b4fc454df33d881
Eugene d'Eon <ejdeon@gmail.com>
2019-12-29 17:42:56 +1300

    derived and validated first 5 event moments for fluence about the 3D point source for arbitrary scattering law - tested for Rayleigh and Lambert Sphere scattering

--- code/3D_medium/infinite3Dmedium/isotropicPointSource/compileandrun.py
+++ code/3D_medium/infinite3Dmedium/isotropicPointSource/compileandrun.py
@@ -16,6 +16,8 @@ os.system('g++ infinite3D_isotropicpoint_LSscatter_exponential.cpp -o infinite3D
 os.system('g++ infinite3D_isotropicpoint_linanisoscatter_exponential.cpp -o infinite3D_isotropicpoint_linanisoscatter_exponential -O3 -I ../../../include/')
 
 cs = [ 0.01, 0.1, 0.3, 0.5, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999 ]
+numorders = 25
+nummoments = 10
 
 # ISOTROPIC, RAYLEIGH & LAMBERT SPHERE SCATTERING
 
@@ -23,13 +25,12 @@ for mfp in [1.0, 0.3]: 		# mean-free path
 	for c in cs: 			# single-scattering albedo
 		nu_0 = 1/math.sqrt(1 - math.pow(c,2.4429445001914587 + 0.5786368322364553/c - 0.021581332427913873*c))
 		expected_num_samples_inv = 1.0 - c
-		numsamples = 10 * min( 10000000, int( 10000000.0 * expected_num_samples_inv ) )
+		numsamples = min( 10000000, int( 10000000.0 * expected_num_samples_inv ) )
 		print 'num samples: ' + str(numsamples)
 		maxr = nu_0 * 25.0 * mfp
 		dr = maxr / 500.0
 		du = 2.0 / 30.0
-		numorders = 50
-		nummoments = 5
+		
 		filename = 'MCdata/inf3d_isotropicpoint_isotropicscatter_c' + str(c) + '_mfp' + str(mfp) + '.txt'
 		print 'computing: ' + filename
 		os.system( './infinite3d_isotropicpoint_isotropicscatter_exponential ' + str(c) + ' ' + str(mfp) + ' ' + str(maxr) + ' ' + str(dr) + ' ' + str(du) + ' ' + str( numsamples ) + ' ' + str( numorders ) + ' ' + str( nummoments ) + ' > ' + filename )
